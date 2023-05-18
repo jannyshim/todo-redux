61df4929bba2b468db95070f2e4c253e1149e0e4
jannyshim <29jannyy@gmail.com>
2023-05-18 18:48:45 +0900

    feat: 투두 리스트 컴포넌트

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,15 +1,114 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import TodoListItem from "./todoListItem";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addTodo,
+  deleteTodo,
+  editTodo,
+  todoChecked,
+  fetchTodosFailure,
+  fetchTodosSuccess,
+} from "../module/todoAction";
+import TodoListItem from "./TodoListItem";
+import Header from "../components/Header";
+import styled from "styled-components";
+
+const TodoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 10px;
+  h1 {
+    text-align: center;
+    margin-bottom: 1rem;
+  }
+`;
 
 const TodoList = () => {
+  const dispatch = useDispatch();
   const allTodos = useSelector((state) => state.todos);
 
-  const MappedTodo = allTodos
-    .sort((a, b) => b.id - a.id)
-    .map((todo, id) => <TodoListItem key={id} todo={todo} />);
+  // 투두 불러오기
+  const fetchTodos = async () => {
+    try {
+      const res = await axios.get("http://localhost:3001/todos");
+      dispatch(fetchTodosSuccess(res.data));
+    } catch (error) {
+      dispatch(fetchTodosFailure(error));
+    }
+  };
+  useEffect(() => {
+    fetchTodos();
+  }, []);
 
-  return <ul>{MappedTodo}</ul>;
+  // 새 투두 등록하기
+  const addTodoList = async (text, select) => {
+    try {
+      const res = await axios.post("http://localhost:3001/todos", {
+        content: text,
+        isChecked: false,
+        importance: select,
+      });
+      dispatch(addTodo(res.data));
+      fetchTodos();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // 투두 수정
+  const handleEditTodo = async (id, editContent) => {
+    try {
+      await axios.patch(`http://localhost:3001/todos/${id}`, {
+        content: editContent,
+      });
+      dispatch(editTodo(id, editContent));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // 투두 삭제
+  const handleDeleteTodo = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3001/todos/${id}`);
+      dispatch(deleteTodo(id));
+      fetchTodos();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // 투두 완료 여부 체크
+  const handleChecked = async (id, isChecked) => {
+    try {
+      await axios.patch(`http://localhost:3001/todos/${id}`, {
+        isChecked: !isChecked,
+      });
+      dispatch(todoChecked(id, isChecked));
+      fetchTodos();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <TodoContainer>
+      <h1>오늘의 할일</h1>
+      <Header onAddTodo={addTodoList} />
+      {allTodos
+        .sort((a, b) => b.id - a.id)
+        .map((todo) => (
+          <TodoListItem
+            key={todo.id}
+            todo={todo}
+            onUpdateTodo={handleEditTodo}
+            onDeleteTodo={handleDeleteTodo}
+            onChecked={handleChecked}
+          />
+        ))}
+    </TodoContainer>
+  );
 };
 
 export default TodoList;
--- src/components/todoListItem.js
+++ src/components/todoListItem.js
@@ -1,44 +1,149 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { deleteTodo, todoChecked } from "../module/todoAction";
+import { selectImportance } from "../module/todoAction";
+import { ImportanceSelect } from "./ImportanceSelect";
+import styled from "styled-components";
 
-const TodoListItem = ({ todo }) => {
-  const dispatch = useDispatch();
-  const { id, content, isChecked } = todo;
+const TodoListItemContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 7px;
+  border-bottom: 1px dashed grey;
+  padding-bottom: 8px;
+  input {
+    border: 1px solid black;
+    border-radius: 3px;
+    height: 1.7rem;
+    margin-right: 3px;
+    margin-left: 3px;
+  }
+  input[type="checkbox"] {
+    appearance: none;
+    margin-top: 4px;
+    width: 1.2rem;
+    height: 1.2rem;
+    border: 2px solid #577d86;
+    border-radius: 50%;
+    cursor: pointer;
+  }
+  input[type="checkbox"]:checked {
+    background-color: #577d86;
+  }
+  select {
+    height: 1.7rem;
+    border-radius: 3px;
+    border: 1px solid #003333;
+  }
+  span {
+    padding-top: 4px;
+    flex-basis: auto;
+    width: fit-content;
+  }
+  .checked {
+    text-decoration: line-through;
+  }
+  // @media ${({ theme }) => theme.windowSize.large} {
+  //   margin-left: 10px;
+  //   font-size: ${({ theme }) => theme.fontSize.lg};
+  // }
+  // @media ${({ theme }) => theme.windowSize.base} {
+  //   margin-left: 10px;
+  //   font-size: ${({ theme }) => theme.fontSize.base};
+  // }
+  // @media ${({ theme }) => theme.small} {
+  //   font-size: ${({ theme }) => theme.fontSize.sm};
+  //   margin-left: 10px;
+  // }
+`;
 
-  const deleteTodoList = async () => {
-    try {
-      await axios.delete(`http://localhost:3001/todos/${id}`);
-      dispatch(deleteTodo(id));
-    } catch (error) {
-      console.log(error);
+const ButtonContainer = styled.span`
+  margin-left: auto;
+  display: flex;
+  gap: 5px;
+  margin-top: -5px;
+  button {
+    color: white;
+    background-color: #9cb4cc;
+    border: none;
+    border-radius: 3px;
+    width: 2.3rem;
+    height: 1.7rem;
+    :hover {
+      transition: 0.3s;
+      background-color: #6699cc;
     }
-  };
-  const handleDeleteClick = () => {
-    deleteTodoList();
-  };
+  }
+`;
 
-  const handleCheckedClick = async () => {
+const TodoListItem = ({ todo, onUpdateTodo, onDeleteTodo, onChecked }) => {
+  const dispatch = useDispatch();
+  const { id, content, isChecked, importance } = todo;
+  const [editContent, setEditContent] = useState(content);
+  const [isEditing, setIsEditing] = useState(false);
+  const [selectedImportance, setSelectedImportance] = useState(importance);
+
+  const handleSelect = async (e) => {
+    setSelectedImportance(e.target.value);
     try {
       await axios.patch(`http://localhost:3001/todos/${id}`, {
-        isChecked: !isChecked,
+        importance: e.target.value,
       });
-      dispatch(todoChecked(id));
+      dispatch(selectImportance(id, e.target.value));
     } catch (error) {
       console.log(error);
     }
   };
+
+  const deleteTodoList = () => {
+    onDeleteTodo(id);
+  };
+
+  const editTodoList = () => {
+    onUpdateTodo(id, editContent);
+    setIsEditing(false);
+  };
+
+  const checkedClick = () => {
+    onChecked(id, isChecked);
+    setIsEditing(false);
+  };
+
+  const handleCancelClick = () => {
+    setEditContent(content);
+    setIsEditing(false);
+  };
+
+  const handleChange = (e) => setEditContent(e.target.value);
+
   return (
-    <li key={id}>
-      <input
-        type="checkbox"
-        defaultChecked={isChecked}
-        onChange={handleCheckedClick}
-      />
-      {isChecked ? <s>{content}</s> : <span>{content}</span>}
-      <button onClick={handleDeleteClick}>삭제</button>
-    </li>
+    <TodoListItemContainer>
+      <ImportanceSelect value={selectedImportance} onChange={handleSelect} />
+      <label htmlFor={`isChecked${id}`}>
+        <input
+          id={`isChecked${id}`}
+          type="checkbox"
+          defaultChecked={isChecked}
+          onChange={checkedClick}
+        />
+      </label>
+      {isEditing ? (
+        <input type="text" value={editContent} onChange={handleChange} />
+      ) : (
+        <span className={isChecked ? "checked" : ""}>{editContent}</span>
+      )}
+      {isEditing ? (
+        <ButtonContainer>
+          <button onClick={editTodoList}>저장</button>
+          <button onClick={handleCancelClick}>취소</button>
+        </ButtonContainer>
+      ) : (
+        <ButtonContainer>
+          <button onClick={() => setIsEditing(true)}>수정</button>
+          <button onClick={deleteTodoList}>삭제</button>
+        </ButtonContainer>
+      )}
+    </TodoListItemContainer>
   );
 };
 export default TodoListItem;
